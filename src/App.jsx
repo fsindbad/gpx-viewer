2df7784e20dfc0d9a2be53a3aa4207c87721be10
fsindbad <160576976+fsindbad@users.noreply.github.com>
2025-07-28 14:38:45 +0200

    fix icon new GPX track

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ import {
 import "leaflet/dist/leaflet.css";
 import GPXParser from "gpxparser";
 
+// ✅ Leaflet icon fix:
 import L from "leaflet";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 delete L.Icon.Default.prototype._getIconUrl;
-
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: markerIcon2x,
   iconUrl: markerIcon,
